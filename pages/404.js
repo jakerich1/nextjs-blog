ecb6653a335385be8d0a55ca77ac42dcdb8e4fe7
jakerich1 <jakeriches1@gmail.com>
2022-01-06 21:22:34 +0000

    updated

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+// generate nextjs 404 page component
+export default function Page404() {
+    return (
+        <>
+            <h1>404</h1>
+            <p>Page not found.</p>
+        </>
+    )
+}
